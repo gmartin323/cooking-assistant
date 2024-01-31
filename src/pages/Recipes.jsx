03aa6e1854d428485658826b64521d9451e12528
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2024-01-31 19:21:21 +0000

    recipe card styling

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -28,15 +28,30 @@ export default function Recipes() {
 
   return (
     <div className='page-container'>
-      <h1>Recipes</h1>
-      <section className='card-container'>
+      <h1 className="page-heading">Recipes</h1>
         {recipesArray[0] && 
-          <div className='recipe-card'>
-            <img className='recipe-card-img' src={recipesArray[0].imageurl}/>
-            <p>{recipesArray[0].name}</p>
-          </div>
-        }
-      </section>
+          <section className='recipe-card-container'>
+            <div className='recipe-card'>
+              <img className='recipe-card-img' src={recipesArray[0].imageurl}/>
+              <div className='recipe-card-info'>
+                <p className='recipe-card-name'> {recipesArray[0].name}</p>
+                <p className='recipe-card-extra-info'> with {recipesArray[0].extra_info}</p>
+                <p className='recipe-card-preptime'>Prep: {recipesArray[0].preparation_time} mins</p>
+                <p className='recipe-card-cooktime'>Cook: {recipesArray[0].cooking_time} mins</p>
+                <p className='recipe-card-serves'>Serves: {recipesArray[0].serves}</p>
+              </div>
+            </div>
+            <div className='recipe-card'>
+              <img className='recipe-card-img' src={recipesArray[0].imageurl}/>
+              <div className='recipe-card-info'>
+                <p className='recipe-card-name'> {recipesArray[0].name}</p>
+                <p className='recipe-card-preptime'>Prep: {recipesArray[0].preparation_time}</p>
+                <p className='recipe-card-cooktime'>Cook: {recipesArray[0].cooking_time}</p>
+                <p className='recipe-card-serves'>Serves: {recipesArray[0].serves}</p>
+              </div>
+            </div>
+        </section>
+      }
     </div>
   )
 }
