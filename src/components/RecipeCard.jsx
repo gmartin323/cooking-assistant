4fb02e433077d2060b8b6b07f23be037b6b47584
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2024-01-31 22:20:47 +0000

    Edited recipe card styling

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,19 +1,18 @@
 import React from "react"
 
-export default function RecipeCard( props ) {
+export default function RecipeCard( { recipe } ) {
 
-  let recipe = {props}
-  console.log(props.recipe)
+  // let ... = { recipe } -> destructure further
 
   return (
     <div className='recipe-card'>
-      <img className='recipe-card-img' src={props.recipe.imageurl}/>
+      <img className='recipe-card-img' src={recipe.imageurl}/>
       <div className='recipe-card-info'>
-        <p className='recipe-card-name'> {props.recipe.name}</p>
-        <p className='recipe-card-extra-info'> with {props.recipe.extra_info}</p>
-        <p className='recipe-card-preptime'>Prep: {props.recipe.preparation_time} mins</p>
-        <p className='recipe-card-cooktime'>Cook: {props.recipe.cooking_time} mins</p>
-        <p className='recipe-card-serves'>Serves: {props.recipe.serves}</p>
+        <p className='recipe-card-name'> {recipe.name}</p>
+        <p className='recipe-card-extra-info'> with {recipe.extra_info}</p>
+        <p className='recipe-card-preptime'>Prep: {recipe.preparation_time} mins</p>
+        <p className='recipe-card-cooktime'>Cook: {recipe.cooking_time} mins</p>
+        <p className='recipe-card-serves'>Serves: {recipe.serves}</p>
       </div>
     </div>
   )
