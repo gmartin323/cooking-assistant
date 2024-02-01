511b5f65081a2808b4c56c4b58e9d490ce32d178
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2024-02-01 23:36:13 +0000

    ingredients list styling

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -8,17 +8,13 @@ import IngredientsList from '../components/IngredientsList'
 export default function Recipe() {
 
   const params = useParams()
-  console.log("params", params)
 
   const recipeData = React.useContext(RecipeDataContext)
 
   const currentRecipe = recipeData.filter((recipe) => recipe.recipeUrl === params.name)[0]
 
-  console.log(currentRecipe)
-
   return (
     <div className='page-container'>
-      <h1>{params.name}</h1>
       <RecipeCard recipe={currentRecipe} />
       <IngredientsList ingredients={currentRecipe.ingredients} />
     </div>
