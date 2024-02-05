43feaa271e2d2014fabac1561b29293b400aed24
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2024-02-05 23:31:56 +0000

    Added click event to mark current direction on recipe page

--- src/components/RecipePageDirections.jsx
+++ src/components/RecipePageDirections.jsx
@@ -2,26 +2,30 @@ import React from "react";
 
 export default function RecipePageDirections( {directions} ) {
 
+  const [currentDirectionIndex, setCurrentDirectionIndex] = React.useState(0)
 
   function pad(num) {
     if (num < 10) {
       return `0${num}` 
     } else {
-      return {
-        num
+      return num
       }
     }
-  }
-
-  console.log(pad(10))
 
   return (
     <section className="recipe-page-directions-container">
-      <h2 className="recipe-page-directions-heading" >Directions</h2>
+      <h2 className="recipe-page-directions-heading">Directions</h2>
       <ul className="recipe-page-directions-list">
         {directions.map((direction, index) => {
           return (
-            <li key={direction} className="recipe-page-direction">
+            <li 
+              key={direction} 
+              className={`
+                recipe-page-direction
+                ${index === currentDirectionIndex ? "currentDirection" : null}
+                `}
+              onClick={() => setCurrentDirectionIndex(index)}
+            >
               <p className="direction-number">{pad(index + 1)}</p>
               <p className="direction-text">{direction}</p>
             </li>
