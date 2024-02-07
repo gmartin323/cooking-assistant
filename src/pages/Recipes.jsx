639cd0f8c2e36b1c03b1bb5a137de37539f518a1
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2024-02-07 22:51:23 +0000

    Margin and padding adjustments to page-container

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -4,7 +4,7 @@ import RecipeCardContainer from '../components/RecipeCardContainer'
 export default function Recipes() {
   return (
       <div className='page-container'>
-        <h1 className="page-heading">Recipes</h1>
+        {/* <h1 className="page-heading">Recipes</h1> */}
         <RecipeCardContainer />
       </div>
   )
